f635b0cd05d77766826cce7d93d8b0a7e010bac3
Nahuel Ourthe <nahuelourthe1996@gmail.com>
2019-05-15 13:44:40 -0300

    Separate the menu code to a single file

---
+++ menu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+# Declare member variables here.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_instances_pressed():
+	get_tree().change_scene("res://examples/instances/instances.tscn")
+
+
+func _on_events_pressed():
+	get_tree().change_scene("res://examples/events/events.tscn")
