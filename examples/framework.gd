4ab7d454cbaf2d3e821b605f0557309a5ef8f3e8
Nahuel Ourthe <nahuelourthe1996@gmail.com>
2019-05-18 02:34:55 -0300

    + reload button to scenes framework and other settings.

--- examples/framework.gd
+++ examples/framework.gd
@@ -3,3 +3,6 @@ extends Control
 # This is called by the "pressed" signal of the button.
 func _on_return_menu_pressed():
 	get_tree().change_scene("res://menu.tscn")
+
+func _on_reload_scene_pressed():
+	get_tree().reload_current_scene()
